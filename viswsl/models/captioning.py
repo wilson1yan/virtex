1abeba8c9baba48af375a629400f6e3ef1d035ef
Karan Desai <kddeskar@gmail.com>
2020-01-21 21:12:48 -0500

    Add base class for TextualStream, early fusion, and VisualMemoryTextualStream.

--- viswsl/models/captioning.py
+++ viswsl/models/captioning.py
@@ -7,74 +7,73 @@ from torch import nn
 
 from viswsl.data.structures import CaptioningBatch
 from viswsl.modules.fusion import Fusion
+from viswsl.modules.textual_stream import TextualStream
+from viswsl.modules.visual_stream import VisualStream
 
 
 class CaptioningModel(nn.Module):
     def __init__(
         self,
-        visual,
-        textual,
-        fusion: Fusion,
-        bidirectional: bool = False,
+        visual: VisualStream,
+        textual: TextualStream,
+        late_fusion: Fusion,
+        is_bidirectional: bool = False,
     ):
         super().__init__()
         self.visual = visual
         self.textual = textual
-        self.fusion = fusion
+        self.late_fusion = late_fusion
 
-        self.bidirectional = bidirectional
+        # Linear layer to project image features to `textual_feature_size` to
+        # facilitate decoder multi-head attention, fusion etc.
+        self.visual_projection = nn.Linear(
+            self.visual.visual_feature_size, self.textual.textual_feature_size
+        )
+        self.output = nn.Linear(
+            self.late_fusion.fused_feature_size, self.textual.vocab_size
+        )
+        self.is_bidirectional = is_bidirectional
+        self.padding_idx = self.textual.padding_idx
 
         # Clone the textual and late fusion modules for backward direction if
-        # doing captioning in both directions (separately).
-        if self.bidirectional:
+        # doing captioning in both directions (separately). o need to clone
+        # early fusion, because direction doesn't play a role at that point.
+        if self.is_bidirectional:
             self.backward_textual = copy.deepcopy(self.textual)
-            self.backward_fusion = copy.deepcopy(self.fusion)
+            self.backward_late_fusion = copy.deepcopy(self.late_fusion)
 
-        self.output: nn.Module = nn.Linear(
-            self.fusion.fused_feature_size, self.textual.vocab_size
-        )
-        self.loss = nn.CrossEntropyLoss(ignore_index=textual.padding_idx)
-
+        self.loss = nn.CrossEntropyLoss(ignore_index=self.padding_idx)
         self._tie_weights()
 
     def _tie_weights(self):
         r"""
         Tie weights at a few places to either save parameters, or simply where
         it makes more sense to have the same weights. For example, tie input
-        and output word embeddings to save parameters. Have a same set of
-        weights to project visual features (agnostic to textual components).
-        This method is only called from :meth:`__init__`. Do not use it from
-        outside the class definition.
+        and output word embeddings to save parameters. This method is only
+        called from :meth:`__init__`. Do not use it from outside.
         """
 
         # Tie input and output word embeddings to reduce parameters.
         # However, output embedding layer will learn its own bias.
-        if self.textual.textual_feature_size == self.fusion.fused_feature_size:
-            self.output.weight = self.textual.embedding.word_embedding.weight
+        if self.textual.textual_feature_size == self.late_fusion.fused_feature_size:
+            self.output.weight = self.textual.embedding.words.weight
         else:
             # Add an intermediate projection layer to `textual_feature_size`
             # if fused features have different size than textual features.
             self.output = nn.Sequential(
                 nn.Linear(
-                    self.fusion.fused_feature_size,
+                    self.late_fusion.fused_feature_size,
                     self.textual.textual_feature_size,
                     bias=False,
                 ),
-                nn.Linear(self.textual.textual_feature_size, self.textual.vocab_size),
+                nn.Linear(
+                    self.textual.textual_feature_size, self.textual.vocab_size
+                ),
             )
             self.output[0].weight.data.normal_(mean=0.0, std=0.02)
-            self.output[-1].weight = self.textual.embedding.word_embedding.weight
+            self.output[-1].weight = self.textual.embedding.words.weight
 
-
-        if self.bidirectional:
-            # Tie the visual projection in fusion for both directions.
-            # DO NOT tie textual projection, because the feature space is
-            # different for different directions.
-            if hasattr(self.fusion, "projections"):
-                self.fusion.projections.visual = (
-                    self.backward_fusion.projections.visual
-                )
-
+        if self.is_bidirectional:
             # Tie word and position embeddings for both directions.
             self.backward_textual.embedding = self.textual.embedding
 
@@ -88,15 +87,21 @@ class CaptioningModel(nn.Module):
             batch["image"].size(0), self.visual.visual_feature_size, -1
         ).permute(0, 2, 1)
 
+        # Now visual and textual features are of same size.
+        # shape: (batch_size, ..., textual_feature_size)
+        projected_visual_features = self.visual_projection(visual_features)
+
         caption_tokens = batch["caption_tokens"]
         caption_lengths = batch["caption_lengths"]
 
         # shape: (batch_size, max_caption_length, textual_feature_size)
-        textual_features = self.textual(caption_tokens, caption_lengths)
-
+        textual_features = self.textual(
+            caption_tokens, caption_lengths, projected_visual_features
+        )
         # shape: (batch_size, max_caption_length, fused_feature_size)
-        fused_features = self.fusion(visual_features, textual_features)
-
+        fused_features = self.late_fusion(
+            projected_visual_features, textual_features
+        )
         # shape: (batch_size, max_caption_length, vocab_size)
         output_logits = self.output(fused_features)
 
@@ -110,13 +115,14 @@ class CaptioningModel(nn.Module):
             "loss_components": {"captioning_forward": loss.clone().detach()},
         }
         # Do captioning in backward direction.
-        if self.bidirectional:
+        if self.is_bidirectional:
             backward_caption_tokens = batch["noitpac_tokens"]
+
             backward_textual_features = self.backward_textual(
-                backward_caption_tokens, caption_lengths
+                backward_caption_tokens, caption_lengths, projected_visual_features
             )
-            backward_fused_features = self.backward_fusion(
-                visual_features, backward_textual_features
+            backward_fused_features = self.backward_late_fusion(
+                projected_visual_features, backward_textual_features
             )
             backward_output_logits = self.output(backward_fused_features)
 
@@ -138,18 +144,16 @@ class CaptioningModel(nn.Module):
         # time-step, and vice-versa.
         if not self.training:
             predictions = torch.argmax(output_logits, dim=-1)[:, :-1]
-            redundant_positions = caption_tokens[:, 1:] == self.textual.padding_idx
-            predictions[redundant_positions] = self.textual.padding_idx
+            redundant_positions = caption_tokens[:, 1:] == self.padding_idx
+            predictions[redundant_positions] = self.padding_idx
             output_dict["predictions"] = {"forward": predictions}
 
-            if self.bidirectional:
+            if self.is_bidirectional:
                 backward_predictions = backward_predictions = torch.argmax(
                     backward_output_logits, dim=-1
-                )
-                backward_predictions[redundant_positions] = self.textual.padding_idx
+                )[:, :-1]
+                backward_predictions[redundant_positions] = self.padding_idx
                 output_dict["predictions"]["backward"] = backward_predictions
-
-            output_dict["predictions"] = predictions
 
         return output_dict
 
@@ -165,18 +169,18 @@ class CaptioningModel(nn.Module):
         predictions_str = ""
         for tokens, preds in zip(batch["caption_tokens"], predictions["forward"]):
             predictions_str += f"""
-                Caption tokens : {tokenizer.decode(tokens)}
-                Predictions (f): {tokenizer.decode(preds)}
+                Caption tokens : {tokenizer.decode(tokens.tolist())}
+                Predictions (f): {tokenizer.decode(preds.tolist())}
 
                 """
 
-        if self.bidirectional:
+        if self.is_bidirectional:
             for tokens, preds in zip(
                 batch["noitpac_tokens"], predictions["backward"]
             ):
                 predictions_str += f"""
-                Noitpac tokens : {tokenizer.decode(tokens)}
-                Predictions (b): {tokenizer.decode(preds)}
+                Noitpac tokens : {tokenizer.decode(tokens.tolist())}
+                Predictions (b): {tokenizer.decode(preds.tolist())}
 
                     """
         return predictions_str
