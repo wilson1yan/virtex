6d7803cd48dbca55bb1c3d6a2eb2f5b5676b55cf
Karan Desai <kddeskar@gmail.com>
2020-02-19 22:18:21 -0500

    Add evaluation for imagenet linear classification protocol.

--- viswsl/data/__init__.py
+++ viswsl/data/__init__.py
@@ -3,12 +3,14 @@ from .datasets.captioning_dataset import (
     CaptioningPretextDataset,
     CocoCaptionsEvalDataset,
 )
+from .datasets.imagenet_dataset import ImageNetDataset
 from .datasets.voc07_dataset import VOC07ClassificationDataset
 from .datasets.word_masking_dataset import WordMaskingPretextDataset
 
 __all__ = [
     "CaptioningPretextDataset",
     "CocoCaptionsEvalDataset",
+    "ImageNetDataset",
     "WordMaskingPretextDataset",
     "VOC07ClassificationDataset",
     "AlexNetPCA",
